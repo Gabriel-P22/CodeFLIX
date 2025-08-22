e8f9553b7a4c310705d2dc5e0068f0e96cdf145c
Gabriel <paschoalsoftwareengineer@gmail.com>
2025-08-22 10:17:41 -0300

    feature: implement validation handler

--- fc3-admin-catalogo/domain/src/test/java/domain/CategoryTest.java
+++ fc3-admin-catalogo/domain/src/test/java/domain/CategoryTest.java
@@ -2,11 +2,14 @@ package domain;
 
 
 import com.fullcycle.admin.catalogo.domain.category.Category;
+import com.fullcycle.admin.catalogo.domain.exceptions.DomainException;
+import com.fullcycle.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CategoryTest {
 
@@ -29,6 +32,26 @@ public class CategoryTest {
         assertNotNull(actualCategory.getCreatedAt());
         assertNotNull(actualCategory.getUpdatedAt());
         assertNull(actualCategory.getDeletedAt());
+    }
+
+
+    @Test
+    public void givenAnInvalidName_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = null;
+        final String expectedErrorMessage = "'name' should no be null";
+        final int expectedErrorCount = 1;
+        final String expectedDescription = "A Categoria mais assistida";
+        final boolean expectedIsActive = true;
+
+        final var actualCategory =
+                Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException = assertThrows(DomainException.class, () -> actualCategory.validate(
+                new ThrowsValidationHandler()
+        ));
+
+        assertEquals(expectedErrorMessage, actualException.getErrors().getFirst().message());
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
     }
 
 }

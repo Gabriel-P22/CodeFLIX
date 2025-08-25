9670f8cdebc905f9efa0532bc5f141b3277bb21d
Gabriel <paschoalsoftwareengineer@gmail.com>
2025-08-25 15:13:59 -0300

    feature: add gateway interface

---
+++ fc3-admin-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/CategoryGateway.java
@@ -0,0 +1,20 @@
+package com.fullcycle.admin.catalogo.domain.category;
+
+import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface CategoryGateway {
+
+    Category create(Category aCategory);
+
+    Category deleteById(CategoryID aCategoryId);
+
+    Pagination<Category> findAll(CategorySearchQuery aQuery);
+
+    Optional<Category> findById(CategoryID aCategoryId);
+
+    Category update(Category aCategory);
+
+
+}
